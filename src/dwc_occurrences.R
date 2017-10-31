2af72621305d667bf2786ecfbd8b2896cc9db729
Peter Desmet <peter.desmet.work@gmail.com>
2017-10-31 17:05:35 +0100

    Fix some typos

--- src/dwc_occurrences.R
+++ src/dwc_occurrences.R
@@ -13,8 +13,7 @@ knitr::opts_chunk$set(echo = TRUE, warning = FALSE, message = FALSE)
 
 #' Set locale (so we use UTF-8 character encoding):
 # This works on Mac OS X, might not work on other OS
-Sys.setlocale("LC_CTYPE", 'English_Australia.1252')
-
+Sys.setlocale("LC_CTYPE", "English_Australia.1252")
 
 #' Load libraries:
 library(tidyverse) # For data transformations
@@ -27,7 +26,7 @@ library(janitor)   # For cleaning input data
 library(knitr)     # For nicer (kable) tables
 
 #' Set file paths (all paths should be relative to this script):
-raw_data_file = "../data/raw/alien_macroinvertebrates_occurences.tsv"
+raw_data_file = "../data/raw/alien_macroinvertebrates_occurrences.tsv"
 
 #' ## Read data
 #' 
@@ -37,14 +36,14 @@ raw_data <- read.table(raw_data_file, header = TRUE, sep = "\t", quote="", fileE
 #' Clean data somewhat: remove empty rows if present
 raw_data %<>%  remove_empty_rows() 
 
-#' Add prefix `raw_` to all column names. Although the column names already contain Darwin Core terms, new columns will have to be added between the current columns. To put all columns in the right order, it is easier create new columns (some of them will be copies of the columns in the raw dataset) and than remove the columns of the raw occurences dataset:
+#' Add prefix `raw_` to all column names. Although the column names already contain Darwin Core terms, new columns will have to be added between the current columns. To put all columns in the right order, it is easier to create new columns (some of them will be copies of the columns in the raw dataset) and then remove the columns of the raw occurrences dataset:
 colnames(raw_data) <- paste0("raw_", colnames(raw_data))
 
 #' Preview data:
 kable(head(raw_data))
 
-#' ## Create occurence core
-occurence <- raw_data
+#' ## Create occurrence core
+occurrence <- raw_data
 
 #' ### Term mapping
 #' 
