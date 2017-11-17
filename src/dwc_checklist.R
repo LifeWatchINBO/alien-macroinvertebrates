71f1c56f79c9cca79041b1f66aa92310e674bd63
Lien Reyserhove <Lien.Reyserhove@inbo.be>
2017-11-17 12:22:05 +0100

    Remove all earlier dwc mapping to start from scratch

--- src/dwc_checklist.R
+++ src/dwc_checklist.R
@@ -26,73 +26,35 @@ library(janitor)   # For cleaning input data
 library(knitr)     # For nicer (kable) tables
 
 #' Set file paths (all paths should be relative to this script):
-raw_data_file = "../data/raw/alien_macroinvertebrates_occurrences.tsv"
-dwc_taxon_file = "../data/processed/dwc_checklist/taxon.csv"
-dwc_distribution_file = "../data/processed/dwc_checklist/distribution.csv"
-dwc_description_file = "../data/processed/dwc_checklist/description.csv"
 
 #' ## Read data
 #' 
 #' Read the source data:
-raw_data <- read.table(raw_data_file, header = TRUE, sep = "\t", quote="", fileEncoding = "UTF-8-BOM") 
-
 #' Clean data somewhat: remove empty rows if present
-raw_data %<>%  remove_empty_rows() 
-
-#' Add prefix `raw_` to all column names. Although the column names already contain Darwin Core terms, new columns will have to be added between the current columns. To put all columns in the right order, it is easier to create new columns (some of them will be copies of the columns in the raw dataset) and then remove the columns of the raw occurrence dataset:
-colnames(raw_data) <- paste0("raw_", colnames(raw_data))
-
 #' Preview data:
-kable(head(raw_data))
-
-#' ## Group by species
-#' 
-#' Group the occurrence data as species data (= one record for each scientific name):
-raw_species <- raw_data # replace by dplyr groupby on taxonID, scientificName, taxonRank and scientificNameAuthorship
-
-#' Order by raw_taxonID:
-# raw_species %<>% arrange(raw_taxonID)
 
 #' Save the raw column names as a list (makes it easier to remove them all later):
-raw_colnames <- colnames(raw_species)
-
 #' Preview data:
 kable(head(raw_species))
 
 #' ## Create taxon core
 #' 
 #' ### Pre-processing
-taxon <- raw_species
-
 #' ### Term mapping
 #' 
 #' Map the source data to [Darwin Core Taxon](http://rs.gbif.org/core/dwc_taxon_2015-04-24.xml):
 #' 
 #' #### modified
 #' #### language
-taxon %<>% mutate(language = "en")
-
 #' #### license
-taxon %<>% mutate(license = "http://creativecommons.org/publicdomain/zero/1.0/")
-
 #' #### rightsHolder
-taxon %<>% mutate(rightsHolder = "Ugent; Aquatic ecolo")
-  
 #' #### accessRights
-taxon %<>% mutate(accessRights = "http://www.inbo.be/en/norms-for-data-use")
-
 #' #### bibliographicCitation
 #' #### informationWithheld
 #' #### datasetID
-taxon %<>% mutate(datasetID = "")
-
 #' #### datasetName
-taxon %<>% mutate(datasetName = "Alien macroinvertebrates checklist for Flanders, Belgium")
-
 #' #### references
 #' #### taxonID
-taxon %<>% mutate(taxonID = "") # Should be taken from source
-
 #' #### scientificNameID
 #' #### acceptedNameUsageID
 #' #### parentNameUsageID
@@ -101,8 +63,6 @@ taxon %<>% mutate(taxonID = "") # Should be taken from source
 #' #### namePublishedInID
 #' #### taxonConceptID
 #' #### scientificName
-taxon %<>% mutate(scientificName = raw_scientificName)
-
 #' #### acceptedNameUsage
 #' #### parentNameUsage
 #' #### originalNameUsage
@@ -111,8 +71,6 @@ taxon %<>% mutate(scientificName = raw_scientificName)
 #' #### namePublishedInYear
 #' #### higherClassification
 #' #### kingdom
-taxon %<>% mutate(kingdom = "Animalia")
-
 #' #### phylum
 #' #### class
 #' #### order
@@ -122,16 +80,10 @@ taxon %<>% mutate(kingdom = "Animalia")
 #' #### specificEpithet
 #' #### infraspecificEpithet
 #' #### taxonRank
-taxon %<>% mutate(taxonRank = raw_taxonRank)
-
 #' #### verbatimTaxonRank
 #' #### scientificNameAuthorship
-taxon %<>% mutate(scientificNameAuthorship = raw_scientificNameAuthorship)
-
 #' #### vernacularName
 #' #### nomenclaturalCode
-taxon %<>% mutate(nomenclaturalCode = "ICZN")
-
 #' #### taxonomicStatus
 #' #### nomenclaturalStatus
 #' #### taxonRemarks
@@ -139,37 +91,22 @@ taxon %<>% mutate(nomenclaturalCode = "ICZN")
 #' ### Post-processing
 #' 
 #' Remove the original columns:
-taxon %<>% select(-one_of(raw_colnames))
-
 #' Preview data:
-kable(head(taxon))
-
 #' Save to CSV:
-write.csv(taxon, file = dwc_taxon_file, na = "", row.names = FALSE, fileEncoding = "UTF-8")
+
+
 
 #' ## Create distribution extension
 #' 
 #' ### Pre-processing
-distribution <- raw_species
-
 #' ### Term mapping
 
 #' #### id
-distribution %<>% mutate(id = "")
-
 #' #### locationID
-distribution %<>% mutate(locationID = "ISO_3166-2:BE-VLG")
-
 #' #### locality
-distribution %<>% mutate(locality = "Flanders")
-
 #' #### countryCode
-distribution %<>% mutate(countryCode = "BE")
-
 #' #### lifeStage
 #' #### occurrenceStatus
-distribution %<>% mutate(occurrenceStatus = "present")
-
 #' #### threatStatus
 #' #### establishmentMeans
 #' #### appendixCITES
@@ -183,15 +120,8 @@ distribution %<>% mutate(occurrenceStatus = "present")
 #' ### Post-processing
 #' 
 #' Remove the original columns:
-
-distribution %<>% select(-one_of(raw_colnames))
-
 #' Preview data:
-kable(head(distribution))
-
 #' Save to CSV:
-write.csv(distribution, file = dwc_distribution_file, na = "", row.names = FALSE, fileEncoding = "UTF-8")
-
 #' ## Summary
 #' 
 #' ### Number of records
